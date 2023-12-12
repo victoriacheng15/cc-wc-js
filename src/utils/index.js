dcbcbe907c937197e3d64eb5c819882d340e8911
Victoria Cheng <victoria.cheng15@Gmail.com>
2023-12-12 09:53:01 -0700

    chore: add index.js in utls folder for ease of export

---
+++ src/utils/index.js
@@ -0,0 +1,11 @@
+import { getStats, getStatsFromData, createOptions, validateFile } from "./process.js";
+import { checkFlag } from "./checkFlag.js";
+
+
+export {
+  getStats,
+  getStatsFromData,
+  createOptions,
+  checkFlag,
+  validateFile
+}
